8db64280cbea9228c7967000303e2c6dc31b15a7
Julia <juliatuvaleva@mail.ru>
2021-03-03 18:37:04 +0300

    index and script connected

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
 const timeCard = document.querySelector('.weather-city__daytime');
 const cityBlock = document.querySelector('.weather-city__city-block');
-
+const cardContainer = document.querySelector('.locations__list');
+const overlay = document.querySelector('.overlay');
+const weatherImg = document.querySelector('.weather-img');
+const weather = [];
 
 function getCurrentTime(textElement) {
     const currentTime =  new Date();
@@ -12,6 +15,75 @@ function getCurrentTime(textElement) {
     currentTime.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
 };
 
+function addCity(name, temperature) {
+    const cardTemplate = document.querySelector('#locations-template').content;
+    const cardElement = cardTemplate.querySelector('.locations__item').cloneNode(true);
+
+
+    const cardCity = cardElement.querySelector('.locations__city');
+    const cardTemperature = cardElement.querySelector('.locations__temp');
+
+    cardCity.textContent = name;
+    cardTemperature.textContent = temperature + String.fromCharCode(176);
+    cardContainer.append(cardElement);
+
+    return cardContainer;
+}
+
+function getCityWeather(name){
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${name}&appid=${api_key}&units=metric`;
+    const tmp = {};
+    
+    fetch(url)
+        .then(function(response){
+            let data =  response.json();
+            return data;
+        })
+        .then(function(data){
+            
+            tmp.name = data.name;
+            tmp.country = data.sys.country;
+            tmp.temperature = Math.floor(data.main.temp);
+            tmp.pressure = data.main.pressure;
+            tmp.humidity = data.main.humidity;
+            tmp.description = data.weather[0].description;
+            tmp.iconId = data.weather[0].icon;
+            addCity(tmp.name, tmp.temperature);
+        });
+    
+    return tmp;
+}
+
+function closePopupWithEsc(evt) {
+    const closeElement = document.querySelector('.overlay_opened');
+    if (evt.key === "Escape") {
+      closePopup(closeElement);
+    }
+};
+
+
+function openPopup(popupElement) {
+    popupElement.classList.add('overlay_opened');
+    document.addEventListener('keydown', closePopupWithEsc);
+} 
+
+function closePopup(popupElement) {
+    popupElement.classList.remove('overlay_opened');
+    document.removeEventListener('keydown', closePopupWithEsc);
+}
+
 getCurrentTime(timeCard);
 
+cityList.forEach(function (item) {
+    weather.push(getCityWeather(item));
+});
 
+
+cityBlock.addEventListener('click', function(){
+    openPopup(overlay);
+});
+
+
+weatherImg.addEventListener('click', function(){
+    closePopup(overlay);
+});
